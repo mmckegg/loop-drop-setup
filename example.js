c4c893aa3239f2668582ebc516269fbbb90f8b18
Matt McKegg <matt@wetsand.co.nz>
2015-01-14 15:47:59 +1300

    add updated example for new version

--- example.js
+++ example.js
@@ -1,96 +1,210 @@
-// external node
-// node inherits from another file and overrides matching params
+var wrapSource = require('audio-slot/source')
+var wrapProcessor = require('audio-slot/processor')
 
-var files = {}
+var Observ = require('observ')
+var Setup = require('./')
+var Bopper = require('bopper')
 
-var context = {
+var audioContext = new AudioContext()
+var scheduler = Bopper(audioContext)
+
+var path = require('path')
+var projectRoot = '/root'
+
+scheduler.setTempo(110)
+scheduler.start()
+
+var context = window.context = {
+
+  audio: audioContext,
+  output: audioContext.createGain(),
+  scheduler: scheduler,
 
   nodes: {
-    launchpad: require('loop-launchpad'),
-    chunk: require('soundbank-chunk'),
-    external: require('loop-drop-setup/external')
+    'looper': require('loop-grid'),
+    'qwerty': require('loop-qwerty'),
+
+    'chunk': require('./chunk'),
+    'external': require('./external'),
+    'slot': require('audio-slot'),
+
+    'oscillator': wrapSource(require('soundbank-oscillator')),
+    'filter': wrapProcessor(audioContext.createBiquadFilter.bind(audioContext)),
+    'overdrive': wrapProcessor(require('soundbank-overdrive')),
+    'sample': wrapSource(require('soundbank-sample')),
+
+    'AudioBuffer': require('loop-drop-project/audio-buffer'),
+
+    'lfo': wrapSource(require('lfo')),
+    'adsr': wrapSource(require('adsr'))
   },
 
-  getFile: watchFile
+  // stub project for sample loading via loop-drop-project/audio-buffer
+  cwd: '/root/setup',
+  project: { 
+    getFile: function(src, type){
 
-}
+      var obs = Observ()
 
-function watchFile(src, cb){
-  if (!files[src]){
-    loadFile(src, function(err, res){ if(err)return cb(err)
-      files[src] = Observ(res)
-      files[src].src = src
-      cb(null, files[src])
-    })
-  } else {
-    cb(null, files[src])
-  }
-}
+      if (src === 'setup/test.wav'){
+        requestArrayBuffer('/sounds/test.wav', function(err, buffer){
+          obs.set(buffer)
+        })
+      }
 
-function saveFile(src, data, cb){
-  if (files[src]){
-    files[src].set(data)
-  }
-  // save the file
-}
+      obs.path = this.resolve(src)
+      obs.src = src
 
-function loadFile(src, cb){
-  // load the file
-  cb(null, res)
-}
+      obs.onClose = function(listener){}
+      obs.close = function(listener){ 
+        obs.onClose()
+        console.log('closed', obs.path)
+      }
 
-
-var setup = {
-  
-  node: 'setup', // loop-drop-setup
-
-  chunks: [
-    { id: 'synth', 
-      node: 'external',
-      src: 'chunks/synth.json',
-      origin: [0,0]
-    }
-  ],
-
-  controllers: [
-
-    { id: 'launchpadA', // loop-grid / launchpad
-      node: 'launchpad',
-      chunkIds: [ 'synth' ]
+      return obs
     },
 
-    { id: 'launchpadB',
-      node: 'launchpad',
-      chunkIds: [ 'synth' ]
+    checkExists: function(src, cb){
+      process.nextTick(function(){
+        if (src === 'setup/test.wav'){
+          cb(null, true)
+        } else {
+          cb(null, false)
+        }
+      })
+    },
+
+    resolve: function(paths){
+      return path.resolve.apply(path, [projectRoot].concat(paths))
+    },
+
+    relative: function(fullPath){
+      return path.relative(projectRoot, fullPath)
     }
-
-  ]
-
-}
-
- 
-chunks = {
-    
-  'synth.json': {
-
-    node: 'chunk', // soundbank-chunk
-    sounds: ['kick', 'snare', 'hihat', 'openhat'],
-    shape: [1,4],
-
-    slots: [
-      {id: 'kick', sources: [{node: 'sample', src: 'samples/kick.wav'}], output: 'post'},
-      {id: 'snare', sources: [{node: 'sample', src: 'samples/snare.wav'}], output: 'post'},
-      {id: 'hihat', sources: [{node: 'sample', src: 'samples/hihat.wav'}], output: 'post'},
-      {id: 'openhat', sources: [{node: 'sample', src: 'samples/openhat.wav'}], output: 'post'},
-      {id: 'post', processors: [{node: 'overdrive'}]}
-    ],
-
-    outputs: ['post']
-
   }
 
 }
 
-samples = {
-  
+context.output.connect(audioContext.destination)
+
+var setup = window.setup = Setup(context)
+
+setup.set({
+  controllers: [
+    //{ node: 'looper', 
+    //  shape: [4,4],
+    //  targets: ['synth/0', 'synth/1', 'synth/2', 'synth/3'],
+    //  loops: [
+    //    {length: 4, events: [[0, 0.5], [1,0.5]]},
+    //    {length: 4, events: [[2, 0.5], [3,0.5]]},
+    //    {length: 8, events: [[0, 2]]},
+    //    {length: 12, events: [[0, 1]]}
+//
+    //  ]
+    //},
+
+    { node: 'qwerty', 
+      chunkPositions: {synth: [1,2]}
+    }
+
+  ],
+
+  chunks: [
+    { id: 'synth',
+      node: 'chunk',
+      shape: [1, 5],
+
+      slots: [
+        { id: '0',
+          node: 'slot',
+          output: 'output',
+          sources: [{ 
+            node: 'oscillator', 
+            shape: 'sawtooth',
+            amp: {
+              node: 'adsr',
+              attack: 1,
+              release: 2
+            },
+            detune: {
+              node: 'lfo',
+              amp: 50,
+              rate: 10
+            }
+          }]
+        },
+        { id: '1',
+          node: 'slot',
+          output: 'output',
+          sources: [{ 
+            node: 'oscillator', note: 73,
+            amp: {
+              node: 'adsr',
+              attack: 1,
+              release: 2
+            } 
+          }]
+        },
+        { id: '2',
+          node: 'slot',
+          output: 'output',
+          sources: [{ node: 'oscillator', note: 73-12+5 }],
+          processors: [{
+            node: 'filter',
+            type: 'highpass',
+            frequency: {
+              node: 'adsr',
+              attack: 10,
+              value: 10000
+            } 
+          }]
+        },
+        { id: '3',
+          node: 'slot',
+          output: 'output',
+          sources: [{ node: 'oscillator', note: 69-24 }]
+        },
+
+        { id: '4',
+          node: 'slot',
+          output: 'output',
+          sources: [{ 
+            node: 'sample', 
+            mode: 'loop',
+            buffer: { node: 'AudioBuffer', src: 'test.wav' },
+            amp: {
+              node: 'adsr',
+              attack: 1,
+              release: 2
+            } 
+          }]
+        },
+
+        { id: 'output',
+          node: 'slot',
+          processors: [{ node: 'overdrive', gain: 10 }]
+        }
+      ],
+
+      flags: {'3': ['noRepeat']},
+      routing: { output: '$default' },
+      outputs: ['output']
+    }
+  ]
+
+})
+
+setup.controllers.get(0).recording(function(grid){
+  console.log(grid.data)
+})
+
+function requestArrayBuffer(url, cb){
+  var request = new window.XMLHttpRequest();
+  request.open('GET', url, true);
+  request.responseType = 'arraybuffer';
+  request.onload = function() {
+    cb(null, request.response)
+  }
+  request.onerror = cb
+  request.send();
 }
