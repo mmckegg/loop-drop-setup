6dde532beada627c03d9229f35e44c7a16839a0d
Matt McKegg <matt@wetsand.co.nz>
2015-02-05 16:39:13 +1300

    add scale-chunk

---
+++ scale-chunk.js
@@ -0,0 +1,168 @@
+var ObservStruct = require('observ-struct')
+var Observ = require('observ')
+var ObservVarhash = require('observ-varhash')
+var NodeArray = require('observ-node-array')
+var ArrayGrid = require('array-grid')
+
+var computed = require('observ/computed')
+var lookup = require('observ-node-array/lookup')
+var merge = require('observ-node-array/merge')
+var watch = require('observ/watch')
+var nextTick = require('next-tick')
+var deepEqual = require('deep-equal')
+var ExternalRouter = require('./external-router')
+
+
+module.exports = ScaleChunk
+
+function ScaleChunk(parentContext){
+
+  var context = Object.create(parentContext)
+
+  context.output = context.audio.createGain()
+  context.output.connect(parentContext.output)
+
+  var scaleSlots = NodeArray(context) 
+
+  var obs = ObservStruct({
+    id: Observ(),
+    shape: Observ(),
+
+    offset: Observ(0),
+    templateSlot: Observ(),
+    scale: Observ(),
+
+    slots: NodeArray(context),
+    inputs: Observ([]),
+    outputs: Observ([]),
+    routes: ExternalRouter(context),
+    flags: ObservVarhash({}),
+    volume: Observ(1),
+    chokeAll: Observ(false),
+    color: Observ([255,255,255]),
+    selectedSlotId: Observ()
+  })
+
+  obs.output = context.output
+  obs.context = context
+
+  context.slotLookup = merge([
+    lookup(scaleSlots, 'id'),
+    lookup(obs.slots, 'id'),
+    singleLookup(obs.templateSlot, 'trigger')
+  ])
+
+  var computedSlots = computed([obs.offset, obs.templateSlot, obs.scale, obs.shape], function(offset, template, scale, shape){
+    var length = shape&&(shape[0]*shape[1])||1
+    var result = []
+    for (var i=0;i<length;i++){
+      var slot = obtainAndSubstitute(template, getNote(scale, i) + offset)
+      if (slot){
+        slot.id = String(i)
+        result.push(slot)
+      }
+    }
+    return result
+  })
+
+  computedSlots(scaleSlots.set)
+
+  obs.triggerOn = function(id, at){
+    var slot = context.slotLookup.get(id)
+
+    if (obs.chokeAll()){
+      obs.slots.forEach(function(slot){
+        slot.choke(at)
+      })
+    }
+
+    if (slot){
+      slot.triggerOn(at)
+    }
+  }
+
+  obs.triggerOff = function(id, at){
+    var slot = context.slotLookup.get(id)
+    if (slot){
+      slot.triggerOff(at)
+    }
+  }
+
+  obs.getSlot = function(id){
+    return context.slotLookup.get(id)
+  }
+
+  obs.triggers = computed([obs.id, obs.shape], function(id, shape){
+    var length = Array.isArray(shape) && shape[0] * shape[1] || 0
+    var result = []
+    for (var i=0;i<length;i++){
+      result.push(String(i))
+    }
+    return result
+  })
+
+  obs.grid = computed([obs.triggers, obs.shape], ArrayGrid)
+
+  obs.resolvedGrid = computed([obs.triggers, obs.shape], function(triggers, shape){
+    return ArrayGrid(triggers.map(getGlobalId), shape)
+  })
+
+  //obs.slots.onUpdate(obs.routes.reconnect)
+  //scaleSlots.onUpdate(obs.routes.reconnect)
+
+  obs.destroy = function(){
+    obs.routes.destroy()
+  }
+
+  return obs
+
+  // scoped
+
+  function getGlobalId(id){
+    if (id){
+      return obs.id() + '/' + id
+    }
+  }
+}
+
+function obtainAndSubstitute(template, offset){
+  return JSON.parse(JSON.stringify(template||{}, function(key, value){
+    if (value && value.$offset){
+      return value.value ? value.value + offset : offset
+    }
+    return value
+  }))
+}
+
+function mod(n, m) {
+  return ((n%m)+m)%m
+}
+
+function getNote(scale, offset){
+  scale = Array.isArray(scale) ? scale : [0,1,2,3,4,5,6,7,8,9,10,11]
+  var position = mod(offset, scale.length)
+  var multiplier = Math.floor(offset/scale.length)
+  return scale[position] + multiplier * 12
+}
+
+function singleLookup(item, key){
+  var obs = Observ([])
+
+  watch(item, function(data){
+    var res = {}
+    res[key] = data
+    obs.set(res)
+  })
+
+  obs.get = function(k){
+    if (k === key){
+      return item
+    }
+  }
+
+  obs.keys = function(){
+    return [key]
+  }
+
+  return obs
+}
